ec7c40a8279471e090786547b37912f6bba5a5bd
Citla <gutierrezcitlali22@gmail.com>
2024-09-23 20:35:17 -0600

    tercer commit lunes 23 filtros

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -10,43 +11,44 @@ export class ProductListComponent {
   imageWith:number=50;
   imageMargin:number=2;
   muestraImg:boolean=true;
+  listFiter:string='';
 
   mostrarImagen():void{
     this.muestraImg=!this.muestraImg
   }
 
-  productos: any[] = [
+  productos: IProductos[] = [
       {
-          "productoId": 1,
-          "modelo": "rubicon",
-          "descripcion": "4 puertas",
-          "year": "enero 15 2020",
-          "precio": 1200000,
-          "marca": "jeep",
-          "color": "negro",
-          "imgUrl": "https://img.remediosdigitales.com/511ec0/jeep-r-wrangler-unlimited-rubicon-edicion-deluxe-2020_2-1-/1366_2000.jpg"
+          "ProductoId": 1,
+          "Modelo": "rubicon",
+          "Descripcion": "4 puertas",
+          "Year": "enero 15 2020",
+          "Precio": 1200000,
+          "Marca": "jeep",
+          "Color": "negro",
+          "ImgUrl": "https://img.remediosdigitales.com/511ec0/jeep-r-wrangler-unlimited-rubicon-edicion-deluxe-2020_2-1-/1366_2000.jpg"
       
       },
       {
-          "productoId": 2,
-          "modelo": "vento",
-          "descripcion": "4 puertas",
-          "year": "mayo 2 2020",
-          "precio": 180000,
-          "marca": "volsvagen",
-          "color": "azul",
-          "imgUrl": "https://img.remediosdigitales.com/6f6fdc/volkswagen-vento-allstar-2017-mexico-2-/1366_2000.jpg"
+          "ProductoId": 2,
+          "Modelo": "vento",
+          "Descripcion": "4 puertas",
+          "Year": "mayo 2 2020",
+          "Precio": 180000,
+          "Marca": "volsvagen",
+          "Color": "azul",
+          "ImgUrl": "https://img.remediosdigitales.com/6f6fdc/volkswagen-vento-allstar-2017-mexico-2-/1366_2000.jpg"
       
       },
       {
-          "productoId": 3,
-          "modelo": "tsuru",
-          "descripcion": "4 puertas",
-          "year": "marzo 12 2021",
-          "precio": 30000,
-          "marca": "nissan",
-          "color": "blanco",
-          "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgEP_vIKAg18pDQ8OTBGyMWgeKJGlqshvs0A&s"
+          "ProductoId": 3,
+          "Modelo": "tsuru",
+          "Descripcion": "4 puertas",
+          "Year": "marzo 12 2021",
+          "Precio": 30000,
+          "Marca": "nissan",
+          "Color": "blanco",
+          "ImgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgEP_vIKAg18pDQ8OTBGyMWgeKJGlqshvs0A&s"
       
       },
       
---
+++ src/app/product/productos.ts
@@ -0,0 +1,10 @@
+export interface IProductos {
+    ProductoId: number;
+    Modelo: string;
+    Descripcion: string;
+    Year: string;
+    Precio: number;
+    Marca: string;
+    Color: string;
+    ImgUrl: string;
+}
